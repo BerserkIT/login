55af041fbc609bfaea530ad22336bea74664ce4d
MVolynyak <volynyak_maksim@mail.ru>
2025-01-19 12:55:48 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Error404 from './pages/Error404/Error404';
+import AppRoutes from './router/routes';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Страница входа */}
-        <Route path="/login" element={<Login />} />
-        {/* Страница 404 */}
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </Router>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
